6d834de0f7b5d61c15833acb92d4c9482cd393d7
Your Name <you@example.com>
2019-11-24 10:49:37 +0000

    pkg

--- env.sh
+++ env.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+linux
+
 export lowRSI=20
 export highRSI=65
 export minCross=0.03
@@ -14,3 +15,21 @@ export stopLoss=-6.5
 export key=yUJluobNfQ5H1nQ9Cp3czdmHL27Wz8I61E1b7tsR2hMYApLCbPbeezvtQWj2D2NL
 export secret=v0BwW14iRs91eppXZYsrqUxYdYTxpWotJNPgpkcph6N3q9mmMOi23BlQrBwSBKZ4
 export min_withdrawal_percent=0.025 
+
+windows
+
+setx lowRSI="20"
+setx highRSI="65"
+setx minCross="0.03"
+setx useMFI="true"
+setx rsiTF="5"
+setx mfiTF="15"
+setx period="54"
+setx kvalue="4"
+setx dvalue="3"
+setx delaybetweenorder"=0.5"
+setx takeProfit="4.5"
+setx stopLoss="-6.5"
+setx key="yUJluobNfQ5H1nQ9Cp3czdmHL27Wz8I61E1b7tsR2hMYApLCbPbeezvtQWj2D2NL"
+setx secret="v0BwW14iRs91eppXZYsrqUxYdYTxpWotJNPgpkcph6N3q9mmMOi23BlQrBwSBKZ4"
+setx min_withdrawal_percent="0.025"
