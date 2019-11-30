696aae1580347e34d1b0ed61673f07e0f2704829
Your Name <you@example.com>
2019-11-30 20:53:08 +0000

    eth

--- env.sh
+++ env.sh
@@ -7,6 +7,13 @@ export secret=secret
 export maxFreePerc=0.6
 export orderSizeMult=2
 export tgUser=JareDunn
+export ethtakeProfit=4.5
+export ethstopLoss=-6.5
+export ethkey=key
+export ethsecret=secret
+export ethmaxFreePerc=0.6
+export ethorderSizeMult=2
+export ethtgUser=JareDunn
 
 
 windows
@@ -18,3 +25,10 @@ setx secret="secret"
 setx maxFreePerc="0.6"
 setx orderSizeMult="2"
 setx tgUser="JareDunn"
+setx ethtakeProfit="4.5"
+setx ethstopLoss="-6.5"
+setx ethkey="key"
+setx ethsecret="secret"
+setx ethmaxFreePerc="0.6"
+setx ethorderSizeMult="2"
+setx ethtgUser="JareDunn"
