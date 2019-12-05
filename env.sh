9febcd8c88d5583e59d143845b8d7f1ab823a0f5
Your Name <you@example.com>
2019-12-05 17:27:27 +0000

    balance.js

--- env.sh
+++ env.sh
@@ -2,8 +2,13 @@ linux
 export delaybetweenorder=0.5
 export takeProfit=4.5
 export stopLoss=-6.5
+export ethdelaybetweenorder=0.5
+export ethtakeProfit=4.5
+export ethstopLoss=-6.5
 export key=
 export secret=
+export ethkey=
+export ethsecret=
 export site=http://coindexbot.duckdns.org
 export port=8080
 
@@ -14,5 +19,10 @@ setx takeProfit "4.5"
 setx stopLoss "-6.5"
 setx key ""
 setx secret ""
+setx ethdelaybetweenorder "0.5"
+setx ethtakeProfit "4.5"
+setx ethstopLoss "-6.5"
+setx ethkey ""
+setx ethsecret ""
 setx site "http://coindexbot.duckdns.org"
 setx port "8080"
