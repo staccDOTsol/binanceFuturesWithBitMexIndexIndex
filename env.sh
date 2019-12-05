0a017924e03cb2a7a694a658c928efcc3067c430
Your Name <you@example.com>
2019-12-05 17:25:01 +0000

    balance.js

--- env.sh
+++ env.sh
@@ -1,39 +1,18 @@
 linux
-
-export lowRSI=20
-export highRSI=65
-export minCross=0.03
-export useMFI=true
-export rsiTF=5
-export mfiTF=15
-export period=54
-export kvalue=4
-export dvalue=3
 export delaybetweenorder=0.5
 export takeProfit=4.5
 export stopLoss=-6.5
-export key=yUJluobNfQ5H1nQ9Cp3czdmHL27Wz8I61E1b7tsR2hMYApLCbPbeezvtQWj2D2NL
-export secret=v0BwW14iRs91eppXZYsrqUxYdYTxpWotJNPgpkcph6N3q9mmMOi23BlQrBwSBKZ4
-export min_withdrawal_percent=0.025 
+export key=
+export secret=
 export site=http://coindexbot.duckdns.org
 export port=8080
 
 windows
 
-setx lowRSI "20"
-setx highRSI "65"
-setx minCross "0.03"
-setx useMFI "true"
-setx rsiTF "5"
-setx mfiTF "15"
-setx period "54"
-setx kvalue "4"
-setx dvalue "3"
 setx delaybetweenorder "0.5"
 setx takeProfit "4.5"
 setx stopLoss "-6.5"
-setx key "yUJluobNfQ5H1nQ9Cp3czdmHL27Wz8I61E1b7tsR2hMYApLCbPbeezvtQWj2D2NL"
-setx secret "v0BwW14iRs91eppXZYsrqUxYdYTxpWotJNPgpkcph6N3q9mmMOi23BlQrBwSBKZ4"
-setx min_withdrawal_percent "0.025"
+setx key ""
+setx secret ""
 setx site "http://coindexbot.duckdns.org"
 setx port "8080"
